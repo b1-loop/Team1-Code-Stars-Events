83a58c341fc379e9a3d77e6fc0730fd32bddbabe
André Pettersson <andre20030417@gmail.com>
2026-01-27 13:43:26 +0100

    skapa databas fil

--- sqleventify/01_create_database.sql
+++ sqleventify/01_create_database.sql
@@ -1,3 +1,16 @@
--- Använd databasen
-USE EventifyDB;
+/*
+  SKRIPT: CreateDatabase.sql
+  BESKRIVNING: Skapar databasen EventifyDB om den inte finns 
+*/
+
+-- Kontrollera om databasen existerar innan vi försöker skapa den
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'EventifyDB')
+BEGIN
+    CREATE DATABASE EventifyDB;
+    PRINT 'Databasen EventifyDB har skapats framgångsrikt.';
+END
+ELSE
+BEGIN
+    PRINT 'Databasen EventifyDB finns redan.';
+END
 GO
